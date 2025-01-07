4b987ca3fe4ea31fe86be5cf6d01bcf03e9e83e1
Poornima Prakash Kaushik <popr01@dfki.de>
2025-01-07 15:50:46 +0100

    Added complete files

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 use std::error::Error;
-use rusty_audio::Audio;
 use std::io;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use std::sync::mpsc::{self, Receiver};
+use std::thread;
+
+use rusty_audio::Audio;
+
 use crossterm::{terminal, ExecutableCommand};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{self, Event, KeyCode};
 
+use project_invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+    render,
+    player::Player,
+    invaders::Invaders,
+};
+
+fn render_screen(render_rx: Receiver<Frame>) {
+    let mut last_frame = frame::new_frame();
+    let mut stdout = io::stdout();
+    render::render(&mut stdout, &last_frame, &last_frame, true);
+    while let Ok(curr_frame) = render_rx.recv() {
+        render::render(&mut stdout, &last_frame, &curr_frame, false);
+        last_frame = curr_frame;
+    }
+}
+
 
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
-    audio.add("explode", "src/explode.wav");
-    audio.add("lose", "src/lose.wav");
-    audio.add("move", "src/move.wav");
-    audio.add("pew", "src/pew.wav");
-    audio.add("startup", "src/startup.wav");
-    audio.add("win","src/win.wav");
+    audio.add("explode", "src/audio/explode.wav");
+    audio.add("lose", "src/audio/lose.wav");
+    audio.add("move", "src/audio/move.wav");
+    audio.add("pew", "src/audio/pew.wav");
+    audio.add("startup", "src/audio/startup.wav");
+    audio.add("win","src/audio/win.wav");
     audio.play("startup");
 
     // terminal
@@ -24,12 +45,34 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move ||{
+        render_screen(render_rx);
+    });
+
     // Game Loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
+
     'gameloop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;                    
@@ -38,9 +81,40 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+        // Updates
+        player.update(delta);
+        if invaders.update(delta){
+            audio.play("move");
+        }
+        let hits: u16 = player.detect_hits(&mut invaders);
+        if hits > 0 {
+            audio.play("explode");
+        } 
+
+        // Draw and render
+        let drawables:Vec<&dyn Drawable> = vec![&player, &invaders];
+        for drawable in drawables{
+            drawable.draw(&mut curr_frame);
+        }
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+
+        // Win or lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
+
     }
 
-    // cleanup
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
